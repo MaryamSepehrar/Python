c0d1cc974dcfd805d6d8dca769d6d5baaff72236
MaryamSepehrar <107502963+MaryamSepehrar@users.noreply.github.com>
2022-07-20 17:34:11 +0200

    Add files via upload

---
+++ roallercoaster.py
@@ -0,0 +1,12 @@
+print("Welcome to rollercoaster!")
+height = int(input("What is your height?"))
+if height > 120:
+  print("You can riderollercoaster!.")
+  age = int(input("What is your age?"))
+  if age <= 18:
+    print(" please pay $7.")
+  else :
+    print("please pay $12.")
+else:
+  print("This is an even number.")
+
